6f4385971372eab4a05319dbd2d4c5f62346e9f2
Summer <victoriasqq@gmail.com>
2019-08-28 11:20:03 +0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Sample
+{
+    public class CodeApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating a code app.");
+        }
+    }
+}
